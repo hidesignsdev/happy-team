3a4ac341cd276ba9e140f5e04728cc3df88a08b7
Ton Nguyen <c2c.dev.003@gmail.com>
2020-03-16 15:05:19 +0700

    create main interface

---
+++ src/components/calculator.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import './button.scss'
+class Calculator extends Component {
+    render() {
+        return (
+            <div id="calculator" className="calculator" >
+                <h3 className="title">Calculator</h3>
+                <div >
+                   <button className="jumbo">AC</button>
+                    
+                   <button>AC</button>
+                   <button>AC</button>
+                   <button>AC</button>
+                   <button>AC</button>
+                   <button>AC</button>
+                   <button>AC</button>
+                   <button>AC</button>
+                   <button>AC</button>
+                   <button>AC</button>
+                   <button>AC</button>
+                   <button>AC</button>
+                   <button>AC</button>
+                   <button>AC</button>
+                   <button>AC</button>
+                   <button className="jumbo">AC</button>
+                   <button>AC</button>
+                   <button>AC</button>
+                </div>
+             
+
+            </div>
+
+        )
+    }
+}
+export default Calculator
