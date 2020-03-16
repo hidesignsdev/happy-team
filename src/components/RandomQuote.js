1507201c6c3a7b34919e3e94cd19a3865bf9af4c
Ton Nguyen <c2c.dev.003@gmail.com>
2020-03-16 12:56:27 +0700

    add fake data by json file

--- src/components/RandomQuote.js
+++ src/components/RandomQuote.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import axios from 'axios';
+// import axios from 'axios';
+import quotes from '../quotes.json'
 class RandomQuote extends Component {
     constructor(props) {
         super(props)
@@ -36,49 +37,9 @@ class RandomQuote extends Component {
     // }
     // fake data
     getQuote(){
-        let quotes = [
-            {
-            "quote" : "Life isn’t about getting and having, it’s about giving and being.",
-            "author": "Kevin Kruse"
-            },
-            {
-            "quote" : "Whatever the mind of man can conceive and believe, it can achieve.",
-            "author" : "Napoleon Hill"
-            },
-            {
-            "quote" : "Strive not to be a success, but rather to be of value.",
-            "author" : "Albert Einstein"
-            },
-            {
-            "quote" : "Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.",
-            "author" : "Robert Frost"
-            },
-            {
-            "quote" : "I attribute my success to this: I never gave or took any excuse.",
-            "author" : "Florence Nightingale"
-            },
-            {
-            "quote" : "You miss 100% of the shots you don’t take.",
-            "author" : "Wayne Gretzky"
-            },
-            {
-            "quote" : "I’ve missed more than 9000 shots in my career. I’ve lost almost 300 games. 26 times I’ve been trusted to take the game winning shot and missed. I’ve failed over and over and over again in my life. And that is why I succeed.",
-            "author" : "Michael Jordan"
-            },
-            {
-            "quote" : "The most difficult thing is the decision to act, the rest is merely tenacity.",
-            "author" : "Amelia Earhart"
-            },
-            {
-            "quote" : "Every strike brings me closer to the next home run.",
-            "author" : "Babe Ruth"
-            },
-            {
-            "quote" : "Definiteness of purpose is the starting point of all achievement.",
-            "author" : "W. Clement Stone"
-            }]
-                let quoteNum = Math.floor(Math.random() * quotes.length) //get random quote number in data
-                let randomQuote = quotes[quoteNum] //actual quote
+        let data = quotes
+                let quoteNum = Math.floor(Math.random() * data.length) //get random quote number in data
+                let randomQuote = data[quoteNum] //actual quote
                 let color = ['red', 'brown', 'Azure', 'orange', 'green', 'Aquamarine', 'pink'] 
                 let colorNum = Math.floor(Math.random() * 7) // get random color    
                 // console.log(color[colorNum])
