56ace17c958218b576141a41a3c7819842de07b4
c2cdev001 <c2c.dev.001@gmail.com>
2020-03-16 09:38:31 +0700

    define redux actions

---
+++ src/redux/actions/constants.js
@@ -0,0 +1,8 @@
+export const ADD = 'ADD'
+export const SUBTRACT = 'SUBTRACT'
+export const MULTIPLY = 'MULTIPLY'
+export const DIVIDE = 'DIVIDE'
+export const INPUT_NUMBER = 'INPUT_NUMBER'
+export const INPUT_DECIMAL = 'INPUT_DECIMAL'
+export const EQUAL = 'EQUAL'
+export const CLEAR_ALL = 'CLEAR_ALL'
