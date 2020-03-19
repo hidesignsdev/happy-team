a73a4261678f2e6af5fe703d0b5bd8f27a524084
Ton Nguyen <c2c.dev.003@gmail.com>
2020-03-19 14:52:51 +0700

    fixed buttons operator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -39,18 +39,21 @@ class Calculator extends Component {
                     <Button display="AC" id="clear" value="clear" className="jumbo" click={this.handleClear} />
                     <Button display="⌫" id="delete" value="delete" />
                     <Button display="/" id="divide" value="/" click={this.handleOperatorInput} />
-                    <Button display="X" id="multiply" value="*" click={this.handleOperatorInput} />
+
                     <Button display="7" id="seven" value="7" click={this.handleNumberInput} />
                     <Button display="8" id="eight" value="8" click={this.handleNumberInput} />
                     <Button display="9" id="nine" value="9" click={this.handleNumberInput} />
-                    <Button display="-" id="subtract" value="-" click={this.handleOperatorInput} />
+                    <Button display="X" id="multiply" value="*" click={this.handleOperatorInput} />
+
                     <Button display="4" id="four" value="4" click={this.handleNumberInput} />
                     <Button display="5" id="five" value="5" click={this.handleNumberInput} />
                     <Button display="6" id="six" value="6" click={this.handleNumberInput} />
-                    <Button display="+" id="add" value="+" click={this.handleOperatorInput} />
+                    <Button display="-" id="subtract" value="-" click={this.handleOperatorInput} />
+
                     <Button display="1" id="one" value="1" click={this.handleNumberInput} />
                     <Button display="2" id="two" value="2" click={this.handleNumberInput} />
                     <Button display="3" id="three" value="3" click={this.handleNumberInput} />
+                    <Button display="+" id="add" value="+" click={this.handleOperatorInput} />
                     <Button display="0" id="zero" value="0" className="jumbo" click={this.handleNumberInput} />
                     <Button display="." id="decimal" value="." click={this.handleDecimalInput} />
                     <Button display="=" id="equals" value="=" className="equalStyle" click={this.handleCalculate} />
