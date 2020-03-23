76d4f798db6f24962b814cfe605f760918da3528
c2cdev001 <c2c.dev.001@gmail.com>
2020-03-23 13:30:16 +0700

    add connect redux to react

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,3 +3,21 @@ export const fetchGithubInfo=(data)=>({
     type:FETCH_GITHUB_INFO,
     payload:data
 })
+
+export const fetchGithubAcc=(username)=>dispatch =>{
+    fetch(
+        `https://api.github.com/users/${username}` 
+        )
+        .then(response => {
+            if (!response.ok) {
+              throw Error(response.statusText);
+            }
+            return response.json();
+          })
+    .then(data=>{
+        dispatch(fetchGithubInfo(data));
+        
+    })
+    .catch(error=>{});
+    
+}
--- src/components/GithubFetchInfo.js
+++ src/components/GithubFetchInfo.js
@@ -1,15 +1,48 @@
 import React, { Component } from 'react';
+import { fetchGithubAcc } from '../actions';
+import {connect} from 'react-redux'
 
 class GithubFetchInfo extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            username:""
+        }
+    }
+    isChange=(event)=>{
+        this.setState({
+            username:event.target.value
+        })
+    }
+    handleFetch=()=>{
+        this.props.fetchingGithubInfo(this.state.username)
+    }
+ 
 
     render() {
+        let githubAccount=this.props.data
+        console.log('account detail:',githubAccount);
+        console.log(this.props.loading)
         return (
             <div>
-                   <input type="text" placeholder="Enter Link User" />
-                    <button >Fetch</button>
+                   <input type="text" placeholder="Enter Username" onChange={(event)=>{this.isChange(event)}}/>
+                    <button onClick={this.handleFetch}>Fetch</button>
             </div>
         );
     }
 }
 
-export default GithubFetchInfo;
+const mapStateToProps = (state, ownProps) => {
+    return {
+        data: state.githubFetch.data,
+        loading:state.githubFetch.loading
+    }
+}
+const mapDispatchToProps = (dispatch, ownProps) => {
+    return {
+        fetchingGithubInfo: (username) => {
+            dispatch(fetchGithubAcc(username))
+        }
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(GithubFetchInfo)
