4f04f76c4262bea894f245a756899d6cfcd528fd
c2cdev001 <c2c.dev.001@gmail.com>
2020-03-23 11:26:51 +0700

    add store and input form

---
+++ src/components/GithubFetchInfo.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class GithubFetchInfo extends Component {
+
+    render() {
+        return (
+            <div>
+                   <input type="text" placeholder="Enter Link User" />
+                    <button >Fetch</button>
+            </div>
+        );
+    }
+}
+
+export default GithubFetchInfo;
