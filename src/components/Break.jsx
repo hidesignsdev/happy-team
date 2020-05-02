0808dc0103c957080751a2c26698718bc2de98dd
c2cdev001 <c2c.dev.001@gmail.com>
2020-05-02 22:50:31 +0700

    add component Timeleft

--- src/components/Break.jsx
+++ src/components/Break.jsx
@@ -1,25 +1,12 @@
-import moment from 'moment';
-import React from 'react';
-import { useState } from 'react';
+import moment from "moment";
+import React from "react";
 
-const Break = () => {
-  const [breakLength, setBreakLength] = useState(300);
-
-  const decrementBreakLengthByOneMinute = () => {
-    const newBreakLength = breakLength - 60;
-
-    if (newBreakLength < 0) {
-      setBreakLength(0);
-    } else {
-      setBreakLength(newBreakLength);
-    }
-  };
-
-  const incrementBreakLengthByOneMinute = () => {
-    setBreakLength(breakLength + 60);
-  };
-
-  const breakLengthInMinutes = moment.duration(breakLength, 's').minutes();
+const Break = ({
+  breakLength,
+  decrementBreakLengthByOneMinute,
+  incrementBreakLengthByOneMinute,
+}) => {
+  const breakLengthInMinutes = moment.duration(breakLength, "s").minutes();
   return (
     <div>
       <p id="break-label">Break</p>
